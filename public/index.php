80512324c2e1a27bccd4944c167224da37a52771
oleksandr-ishchuk <alexmway@mail.ru>
2024-01-21 22:20:32 +0100

    add docker-compose

--- public/index.php
+++ public/index.php
@@ -3,7 +3,18 @@ include "../src/autoload.php";
 
 use tokenization\TokenArray;
 
-$returnTocken = new TokenArray("2 + 45 - 13 * 56 - 88 / 5 + 2");
-echo $returnTocken->getRez();
+echo '<h2>Interpreter design pattern used</h2>';
+echo '<p>Leson: 2 + 45 - 13 * 56 - 88 / 5 + 2</p>';
+echo '<p>There must be a space between numbers and characters</p>';
+
+echo '<form>';
+echo '<input type="text" name="calc" placeholder="2 + 2">';
+echo '<input type="submit" value="calculate">';
+echo '</form>';
+
+if (isset($_GET['calc'])) {
+    $returnTocken = new TokenArray($_GET['calc']);
+    echo $returnTocken->getRez();
+}
 
 
